5ea441ad663ee1700b4afa83ede2d4907a7f08f2
RachitKaushik <93470513+RachitKaushik@users.noreply.github.com>
2023-05-03 14:53:02 +0530

    weather application

---
+++ weather-app/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor(private weatherService: WeatherService){}
+
+  weatherData?: WeatherData;
+  
+  ngOnInit(): void {
+
+    this.weatherService.getWeatherData('washington')
+    .subscribe({
+      next:(response)=>{
+
+        this.weatherData = response;
+        console.log(response)
+
+      }
+    })
+    
+  }
+  title = 'weather-app';
+}
---
+++ weather-app/src/app/environment/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production:false,
+    weatherApiBaseUrl:'https://open-weather13.p.rapidapi.com/city/delhi',
+    XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue:'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue:'59e63ac93dmsh5bd37e493de1b95p1c45c6jsn2c8fb4cae55a'
+
+
+}
